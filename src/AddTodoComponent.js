4f144d95beb88e827953575459ddcff3feb25a27
venkat <venkatchala2@gmail.com>
2022-02-24 15:49:05 +0530

    display list in  paragraph

--- src/AddTodoComponent.js
+++ src/AddTodoComponent.js
@@ -1,28 +1,28 @@
 import { useState } from "react";
+import App from "./App";
 
 function AddTodoComponent() {
     const [value, setValue] = useState(null);
-    const [list, setList] = useState([]);
+    const [list, setList] = useState(null);
+
 
     const onClickEvent = (e) => {
         e.preventDefault();
 
 
-        setList([...list, value])
+        setList(list, value)
     }
     return (
         <div>
             <h1>Add Task To-Do</h1>
             <input type="text" name="inputValue" onChange={e => setValue(e.target.value)}></input>
             <button onClick={onClickEvent}> Add</button>
-
-            {list &&
-                list.map((val) => {
-                    return <p>{val}</p>
-                })
+            <ul>
+                <App todolist={list}></App>
+            </ul>
 
 
-            }
+
         </div>
     );
 }
--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
 
-function App() {
+function App(props) {
+  const [list, setList] = useState([props.todoList]);
+
+  useEffect(() => {
+    setList([...list, list])
+    list &&
+      list.map((val) => {
+        return <p>{val}</p>
+      })
+  })
+
+
+
+
   return (
     <div >
+
+      <li>{list}</li>
+
 
     </div>
   );
