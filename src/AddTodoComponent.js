f7ec1ba4ea751a9e466b80392a731fdac7fd5c71
venkatachalam <venkatchala2@gmail.com>
2022-03-04 08:34:47 +0530

    Updated files with changes

--- src/AddTodoComponent.js
+++ src/AddTodoComponent.js
@@ -1,17 +1,65 @@
-import { type } from "@testing-library/user-event/dist/type";
-import { useState } from "react";
-import App from "./App";
+import React, { useState, useEffect } from "react";
+import './AddTodoComponent.css'
 
 function AddTodoComponent() {
-    const [value, setValue] = useState({ value: "", status: "" });
+    const [value, setValue] = useState({ value: "", status: null });
     const [list, setList] = useState([]);
+    const [isLoaded, setIsLoaded] = useState(false);
+    
+    
 
+useEffect(() =>{
+    if(!isLoaded){
+        setIsLoaded(true);
+        setList(JSON.parse(localStorage.getItem('TodoList')))
+        
+    }else{
+        localStorage.setItem('TodoList', JSON.stringify(list));
+    }
+  
+
+})
 
     const onClickEvent = (e) => {
         e.preventDefault();
-        setList([...list, value]);
+        let x = list.map((name) => name.value).indexOf(value.value)
+        if(x === -1){
+           
+            setList([...list, value]);
+            
+        } else{
+            alert("The value you entered is Already present");
+        }
+              
+       
     }
 
+    const onChangeCheckbox = (inputId) =>{
+         
+        
+           const updatedList = list.map((val, index) => {
+                 if(index === inputId){
+                      (val.status === 0 )? val.status = 1 : val.status = 0;
+                                     
+                      
+                }
+                return val
+            })
+            setList(updatedList);
+         
+        }
+        const onClickDelete = (inputId) =>{
+            const updatedList = list.filter((val, index) =>{
+                if(index === inputId){
+                    localStorage.removeItem('TodoList')
+                    return false;
+                }
+                    return val
+                
+            })
+            setList(updatedList);
+              
+        }
     return (
         <div>
             <h1>Add Task To-Do</h1>
@@ -19,17 +67,18 @@ function AddTodoComponent() {
             <button onClick={onClickEvent}> Add</button>
             <ul>
                 {list &&
-                    list.map((val) => {
-                        return <li>{val.value}</li>
+                    list.map((val, index) => {
+                        const todoClass = (val.status === 0)?  "InCompleteTodo": "CompletedTodo";
+                        return<li style={{listStyleType:"none"}} key={index} >
+                            <input type="checkbox"  id={index} name={val.status} onChange={(e) => onChangeCheckbox(index)}/>
+                            <span className={todoClass}>{val.value}</span><button id={index} onClick={(e) => onClickDelete(index)}>X</button>
+                            </li>
 
                     })
-
                 }
             </ul>
-
-
-
         </div>
     );
 }
+
 export default AddTodoComponent;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 import AddTodoComponent from './AddTodoComponent';
-import GetDetails from './GetDetails';
 
 ReactDOM.render(
   <div>
 
-    <GetDetails />
+    <AddTodoComponent />
 
   </div>,
 
